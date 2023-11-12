b905385b5665ecd013b258042a697f1a1b30a9a0
Abdullah <64988475+tttdsg@users.noreply.github.com>
2023-11-12 21:31:08 +0200

    Add files via upload

---
+++ Createcs/1.0.1/Createcs.psm1
@@ -0,0 +1,41 @@
+function New-Cs {
+
+ Param (
+ [Parameter(Position=0,mandatory=$true)][string]$Name,
+ [string]$Path,
+ [string]$MainPath
+ )
+
+    $UserPath = "$env:UserProfile"
+    
+ if ($MainPath -ne ""){
+
+    Export-Clixml -InputObject $MainPath -Path "$Userpath\Documents\PowerShell\Modules\createcs\MainPath.clixml"
+ } 
+
+    $tp = Test-Path -Path "$Userpath\Documents\PowerShell\Modules\createcs\MainPath.clixml"
+
+
+
+ if ($Path -eq ""){
+
+    if ($tp -eq $false) {
+
+        $Path = [string](Read-Host -Prompt "Please enter the desired path: ")
+    }
+
+    Else {
+
+        $mp = Import-Clixml -Path "$Userpath\Documents\PowerShell\Modules\createcs\MainPath.clixml"
+        $Path = $mp
+    }
+
+ }
+ 
+ New-Item -ItemType Directory -Path "$Path" -Name "$name"
+
+ Set-Location "$Path\$name"
+ 
+ dotnet new console 
+
+}
